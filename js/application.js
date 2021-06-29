a5fc9f7555a3cc0fd455893bf3c2b41d1c06b734
Schneur <schneuryiftach@gmail.com>
2021-06-29 11:39:30 -0400

    updated so user can add his own items to list and remove items from list

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+var updateCosts = function () {
 
   var sum = function (acc, x) { return acc + x; };
 
@@ -8,11 +8,45 @@ $(document).ready(function () {
     var price = Number($(ele).children('.price').text());
     var quantity = Number($(ele).find('input').val());
     var subtotal = price * quantity;
-    $(ele).children('.subTotalPrice').html(subtotal);
+    $(ele).children('.subTotalPrice').html(subtotal + ".00");
     totals.push(subtotal);
     console.log(totals);
   });
-  
+
   var subtotals = totals.reduce(sum);
-  $('#total').html(subtotals);
-});       
+  $('#total').html(subtotals + ".00");
+
+}
+
+$(document).ready(function () {
+  updateCosts();
+
+  $(document).on('click', '.btn.remove', function (event) {
+    $(this).closest('tr').remove();
+    updateCosts();
+  });
+  var timeout;
+  $(document).on('input', 'tr input', function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      updateCosts();
+    }, 535);
+  }); 
+
+  $('#addStock').on('submit', function (event) {
+    event.preventDefault();
+    var name = $(this).children('.itemName').val();
+    var price = $(this).children('.price').val();
+    var quantity = $(this).children('.quantity').val();
+  
+    $('tbody').append('<tr>' +
+      '<td class="name">' + name + '</td>' +
+      '<td class="price">' + price + '</td>' +
+      '<td class="quantity"><input type="number" value="' + quantity + '" /></td>' +
+      '<td class="subTotalPrice"></td>' +
+      '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
+    '</tr>');
+    updateCosts();
+  });
+});   
+
